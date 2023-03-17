3751c81e76d4b403d703c20238551fc9ebe2193d
Master8631 <murigipeternjogu254@gmail.com>
2023-03-17 09:06:50 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio-h>
+	int main()
+{
+	int (int, char, long int, long long int, float)
+
+	printf(sizeof(int));
+	printf(sizeof(char));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	printf(sizeof(float));
+	return(0);
+}
