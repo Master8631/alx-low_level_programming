b464117ada44b3b5dd7a4ca4f410bb9d62319a38
Master8631 <murigipeternjogu254@gmail.com>
2023-03-24 04:31:38 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9 followed by a new line
+ * @x: prints new lines
+ * Return: prints 0 to 9 and a new line
+ */
+void print_numbers(void)
+
+{
+	{
+	_putchar (48 >= x && x <= 57);
+	}
+	return (0);
+}
