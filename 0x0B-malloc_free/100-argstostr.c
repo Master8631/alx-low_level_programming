2741334ea8e3bdd01db7243857765fb3e265ceac
Master8631 <murigipeternjogu254@gmail.com>
2023-04-12 19:28:55 -0700

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
+ * @cd: int input
+ * @fm: double pointer array
  * Return: 0
  */
 char *argstostr(int cd, char **fm)
